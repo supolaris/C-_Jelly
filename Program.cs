2c50aabfffce09f4e8e077773ae8a1a44aba0ab0
Muhammad Suleman <msulaman061@gmail.com>
2023-10-23 16:12:32 +0500

    data types input

--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace C__Jelly
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            //String input
+            Console.WriteLine("Getting input as a string");
+            string myWord = Console.ReadLine();
+            Console.WriteLine(myWord);
+
+            //Integer input
+            Console.WriteLine("Getting input as a integer");
+            int myIntNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(myIntNum);
+
+            //Float input
+            Console.WriteLine("Getting input a a float");
+            float myFloatNum = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(myFloatNum);
+
+            Console.ReadKey();
+            
         }
     }
 }
