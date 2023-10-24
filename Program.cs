ac1cfc00c40460c585f294615b84886513ad1363
Muhammad Suleman <msulaman061@gmail.com>
2023-10-24 17:46:45 +0500

    short hand if else

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace C__Jelly
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("Short hand if Else");
+
+            int myWord = 627;
+            int rollNumber = 27;
+            //this is called short hand if else
+            string answer = (myWord > rollNumber) ? "myword is greater" : "myWord is not greate";
+            Console.WriteLine("The answer " + answer);
         }
     }
 }
