54ad1498126c1159497f415774ebdd3216bad003
Muhammad Suleman <msulaman061@gmail.com>
2023-10-25 14:18:24 +0500

    value from array

--- Program.cs
+++ Program.cs
@@ -10,7 +10,15 @@ namespace C__Jelly
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("Value from an Array");
+            int[,] myAray = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 3; j++) {
+                    Console.WriteLine(myAray[i,j]);    
+                }
+            }
+
         }
     }
 }
