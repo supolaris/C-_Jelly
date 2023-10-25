64dc7145c35ce1aa68f93751432adcefa7d41cc3
Muhammad Suleman <msulaman061@gmail.com>
2023-10-25 10:20:23 +0500

    update

--- Program.cs
+++ Program.cs
@@ -8,9 +8,25 @@ namespace C__Jelly
 {
     internal class Program
     {
+        private static object toint32;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.WriteLine("Billing Program using switches");
+
+            Console.WriteLine("Enter Your name");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Enter your bill amount");
+            int myBill = Convert.ToInt32(Console.ReadLine());
+            int myMaxValue = 1000;
+            int myMinValue = 10000;
+
+            if (myBill >= myMaxValue)
+            {
+                Console.WriteLine("Message here");
+                myBill += 200*1000/myBill;
+                Console.WriteLine(myBill);
+            }
         }
     }
 }
